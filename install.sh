e7a0fb7b90228f70f3e3484cf310c283ec838ce2
Justaus3r <72691864+Justaus3r@users.noreply.github.com>
2021-12-18 21:49:40 +0000

    Chore:Reflect changes in install script

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 # A terrible user-script for autamating stuff
 
-NVIM_CONF_PATH="$HOME/.config/nvim"
+NVIM_CONF_PATH="$HOME/.config/nvim/"
 PLUGGED_PATH="$HOME/.local/share/nvim/site/autoload/plug.vim"
 
 function init_nvim_conf {
     if [ -d $NVIM_CONF_PATH ];then
-	cp init.vim $NVIM_CONF_PATH
+	cp -r configs/ init.vim $NVIM_CONF_PATH
     else
 	printf "Nvim config directory not found,creating one..\n"
 	mkdir $NVIM_CONF_PATH
-        cp init.vim $NVIM_CONF_PATH
+        cp -r configs/ init.vim $NVIM_CONF_PATH
    fi
 }
 
@@ -45,4 +45,5 @@ init_nvim_conf
 validate_runtime_dependencies
 validate_vim-plug
 printf "Done..\n"
+
 
